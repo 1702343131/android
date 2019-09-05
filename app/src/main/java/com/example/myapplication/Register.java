dd56b1c181b95e16a1e985e253c11e58c9402bc5
张文旭 <zhangwenxu@163.com>
2019-09-05 13:34:22 +0800

    second

--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -2,13 +2,19 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 import com.example.myapplication.utils.MD5Utils;
 
 public class Register extends AppCompatActivity {
@@ -18,13 +24,18 @@ public class Register extends AppCompatActivity {
     // 3.1获取控件的值
     //3.2检查数据的有效性
     //3.3将注册信息存储
+    private Toolbar toolbar;
     private EditText etUsername, etPaaword, etPwdAgain;
     private Button btnRegister;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initToolbar();//初始化toolbar
+         initView();//初始化界面控件
+         initData();//初始化传入的数据
 
         //1. 获取界面上的控件
         initView();
@@ -58,6 +69,38 @@ public class Register extends AppCompatActivity {
         });
     }
 
+    private void initData() {
+        String username = readPres();
+        if (!TextUtils.isEmpty(username)){
+            etUsername.setText(username);
+        }
+    }
+
+    private String readPres() {
+        SharedPreferences sp = getSharedPreferences("userInfo",MODE_PRIVATE);
+        return sp.getString("username","");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.title_toolbar);
+        toolbar.setTitle("注册");
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+                   actionBar.setDisplayHomeAsUpEnabled(true);//设置返回键
+            //actionBar.setHomeButtonEnabled(true);//设置是否为首页
+
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void  onClick(View v){
+                Register.this.finish();
+            }
+        });
+    }
+
+
     private void savePref(String username, String password) {
         SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
         editor.putString("username", username);
@@ -70,5 +113,15 @@ public class Register extends AppCompatActivity {
         etPaaword = findViewById(R.id.password);
         etPwdAgain = findViewById(R.id.passwordAgain);
         btnRegister = findViewById(R.id.register);
+
+        TextView tvRegister = findViewById(R.id.register);
+        tvRegister.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public  void  onClick(View v){
+                 Intent intent = new Intent( Register.this,MainActivity.class);
+                 startActivity(intent);
+
+            }
+        });
     }
         }
