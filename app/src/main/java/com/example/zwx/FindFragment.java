80c8a27b8c2175a017ab36ab9d9c2b9d247b2f2a
张文旭 <zhangwenxu@163.com>
2019-09-09 17:48:50 +0800

    bar

---
+++ app/src/main/java/com/example/zwx/FindFragment.java
@@ -0,0 +1,14 @@
+package com.example.zwx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_fragment);
+    }
+}
